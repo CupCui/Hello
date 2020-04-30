f6acb3e5050154b251039c44a52ad573adac4662
garre <garrettcui@outlook.com>
2020-04-30 17:05:06 +0800

    add redirect to https://acupxxx.github.io/

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@ export default {
     HelloWorld
   },
   created() {
-  	this.$router.push({path:'https://acupxxx.github.io/'})
+  	this.$router.push({path:'https://acupxxx.github.io'})
   }
 }
 </script>
